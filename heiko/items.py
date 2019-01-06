a424636f6cd12479592ea45f11cae01a8a12e89f
Florian Baumann <flo@noqqe.de>
2019-01-06 15:15:06 +0100

    Moved create_item to heiko.items

--- heiko/items.py
+++ heiko/items.py
@@ -48,3 +48,38 @@ def consume_item(auth, client, itemid):
     except:
         menu.log("Something went wrong, contact developer!")
         return False
+
+def create_item(auth, client):
+    """
+    Asks admin for details and creates new item in the backend
+
+    :auth: dict
+    :returns: bool
+    """
+
+    name = input("Name of Drink: ")
+    if not all(c in ascii_letters+'-' for c in name):
+        menu.log("Username not valid. Please be alphanumerical.", serv="ERROR")
+        return False
+
+    if len(name) > 32:
+        menu.log("Name of item is too long (max: 32)", serv="ERROR")
+        return False
+
+    if len(name) < 3:
+        menu.log("Name of item is too short (min: 3)", serv="ERROR")
+        return False
+
+    cost = float(input("Price in EUR: ")) * 100
+
+    if cost < 0:
+        menu.log("Negative price is not allowed ;)", serv="ERROR")
+        return False
+
+    try:
+        client.items_post(name, cost)
+    except:
+        menu.log("Item could not be created in the backend", serv="ERROR")
+        return False
+
+    return True
