d4a3ec97fc506629a45a189149cecc10c16612a6
Florian Baumann <flo@noqqe.de>
2019-03-07 21:17:15 +0100

    fixes voice

--- heiko/voice.py
+++ heiko/voice.py
@@ -5,15 +5,15 @@ def say(what):
     pygame.mixer.init()
 
     if what == "welcome":
-        pygame.mixer.music.load("./sound/welcome.mp3")
+        pygame.mixer.music.load("./heiko/sound/welcome.mp3")
     elif what == "error":
-        pygame.mixer.music.load("./sound/error.mp3")
+        pygame.mixer.music.load("./heiko/sound/error.mp3")
     elif what == "cheers":
-        pygame.mixer.music.load("./sound/cheers.mp3")
+        pygame.mixer.music.load("./heiko/sound/cheers.mp3")
     elif what == "quit":
-        pygame.mixer.music.load("./sound/quit.mp3")
+        pygame.mixer.music.load("./heiko/sound/quit.mp3")
     elif what == "transaction_successful":
-        pygame.mixer.music.load("./sound/transaction_successful.mp3")
+        pygame.mixer.music.load("./heiko/sound/transaction_successful.mp3")
 
     try:
         pygame.mixer.music.play()
