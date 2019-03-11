817c49c3e9b96ac449d65a46e12f571f073d6788
Florian Baumann <flo@noqqe.de>
2019-03-11 20:55:46 +0100

    converted everything to wav files

--- heiko/voice.py
+++ heiko/voice.py
@@ -6,15 +6,15 @@ def say(what):
         pygame.mixer.init()
 
         if what == "welcome":
-            pygame.mixer.music.load("./heiko/sound/welcome.mp3")
+            pygame.mixer.music.load("./heiko/sound/welcome.wav")
         elif what == "error":
-            pygame.mixer.music.load("./heiko/sound/error.mp3")
+            pygame.mixer.music.load("./heiko/sound/error.wav")
         elif what == "cheers":
-            pygame.mixer.music.load("./heiko/sound/cheers.mp3")
+            pygame.mixer.music.load("./heiko/sound/cheers.wav")
         elif what == "quit":
-            pygame.mixer.music.load("./heiko/sound/quit.mp3")
+            pygame.mixer.music.load("./heiko/sound/quit.wav")
         elif what == "transaction_successful":
-            pygame.mixer.music.load("./heiko/sound/transaction_successful.mp3")
+            pygame.mixer.music.load("./heiko/sound/transaction_successful.wav")
 
         pygame.mixer.music.play()
     except:
